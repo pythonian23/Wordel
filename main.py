23334d961d85a3b15656c04524d2f0fdede70a37
pythonian23 <pythonian23@gmail.com>
2022-05-06 21:09:16 +0900

    improvements

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
+#!/usr/bin/python3
+
 from wordel import Wordel
 import argparse
 
 
 parser = argparse.ArgumentParser(description="Wordle but bad.")
-parser.add_argument("--hard", action="store_true", help="Enable hard mode.")
-parser.add_argument("--debug", action="store_true", help="Debug.")
-parser.add_argument("--ans", default=None, help="Set the answer to a specific word.")
+parser.add_argument("--hard", "-H", action="store_true", help="Enable hard mode")
+parser.add_argument("--debug", "-d", action="store_true", help="Debug")
+parser.add_argument("--answer", "-A", default=None, help="Set the answer to a specific word")
+parser.add_argument("--chars", "-c", default=5, help="The number of characters per row")
+parser.add_argument("--attempts", "-a", default=-1, help="The number of attempts to allow")
 args = parser.parse_args()
 
-w = Wordel(hard=args.hard, debug=args.debug)
-w.run(ans=args.ans)
+w = Wordel(hard=args.hard, debug=args.debug, length=int(args.chars), attempts=int(args.attempts))
+w.run(ans=args.answer)
--- wordel.py
+++ wordel.py
@@ -8,7 +8,17 @@ import hashlib
 import utils
 
 
+# Constants for color
+WHITE = colorama.Fore.WHITE
+GREY = colorama.Fore.WHITE + colorama.Style.DIM
+YELLOW = colorama.Fore.YELLOW + colorama.Style.BRIGHT
+GREEN = colorama.Fore.GREEN + colorama.Style.BRIGHT
+
+
 def get_words(source, length, allowed, mod=(lambda x: x)):
+    """
+    get_words
+    """
     cache_file = "cache/" + hashlib.sha256(source.encode("ascii")).hexdigest() + ".cache"
     if os.path.exists(cache_file):
         file = open(cache_file).read()
@@ -23,8 +33,9 @@ def get_words(source, length, allowed, mod=(lambda x: x)):
         invalid = False
 
         invalid = invalid or len(word) != length
-        for char in word:
-            invalid = invalid or (char not in allowed)
+        if not invalid:
+            for char in word:
+                invalid = invalid or (char not in allowed)
         if invalid:
             continue
         words.append(word)
@@ -33,14 +44,15 @@ def get_words(source, length, allowed, mod=(lambda x: x)):
 
 class Wordel:
     def __init__(self, *args, **kwargs):
-        source, self.length, self.allowed, self.hard, self.debug \
+        source, self.length, self.allowed, self.hard, self.debug, self.attempts \
             = utils.kwarg_parser({
-                "source": "https://raw.githubusercontent.com/dwyl/english-words/master/words_alpha.txt",
-                "length": 5,
-                "allowed": "qwertyuiopasdfghjklzxcvbnm",
-                "hard": False,
-                "debug": False
-            }, **kwargs)
+            "source": "https://raw.githubusercontent.com/dwyl/english-words/master/words_alpha.txt",
+            "length": 5,
+            "allowed": "qwertyuiopasdfghjklzxcvbnm",
+            "hard": False,
+            "debug": False,
+            "attempts": -1,
+        }, **kwargs)
         self.words = get_words(source, self.length, self.allowed, mod=(lambda x: x.lower()))
 
     def run(self, ans=None):
@@ -54,19 +66,19 @@ class Wordel:
                 elif not data[i] == char:
                     return False
             return True
+
         guesses = []
         known = []
+        word: str
         if ans is None:
             word = random.choice(self.words)
         else:
             word = ans
         utils.clear()
         i = 0
-        WHITE = colorama.Fore.WHITE
-        GREY = colorama.Fore.WHITE+colorama.Style.DIM
-        YELLOW = colorama.Fore.YELLOW+colorama.Style.BRIGHT
-        GREEN = colorama.Fore.GREEN+colorama.Style.BRIGHT
-        keyboard = {c:WHITE for c in self.allowed}
+
+        keyboard = {c: WHITE for c in self.allowed}
+
         def upgrade_keyboard(key, color):
             if keyboard[key] == WHITE:
                 keyboard[key] = color
@@ -74,51 +86,65 @@ class Wordel:
                 keyboard[key] = color
             elif keyboard[key] == YELLOW and color != GREY and color != WHITE:
                 keyboard[key] = color
+
         def format_keyboard():
             out = []
             items = list(keyboard.items())
-            out.append(" ".join([color+key+colorama.Style.RESET_ALL for key, color in items[0:len(items)//3+2]]))
-            out.append(" "+" ".join([color+key+colorama.Style.RESET_ALL for key, color in items[len(items)//3+2:2*(len(items)//3)+3]]))
-            out.append("  "+" ".join([color+key+colorama.Style.RESET_ALL for key, color in items[2*(len(items)//3)+3:]]))
+            out.append(
+                " ".join([color + key + colorama.Style.RESET_ALL for key, color in items[0:len(items) // 3 + 2]]))
+            out.append(" " + " ".join([color + key + colorama.Style.RESET_ALL for key, color in
+                                       items[len(items) // 3 + 2:2 * (len(items) // 3) + 3]]))
+            out.append("  " + " ".join(
+                [color + key + colorama.Style.RESET_ALL for key, color in items[2 * (len(items) // 3) + 3:]]))
             return "\n".join(out)
-        show = lambda: word*int(self.debug)+str(known)*int(self.debug)+colorama.Fore.BLUE+"="*(self.length*2-1)+colorama.Style.RESET_ALL+"\n"+"\n".join(guesses + [" ".join([WHITE+"-"+colorama.Style.RESET_ALL]*(self.length)) for _ in range(remaining)])+"\n"+colorama.Fore.BLUE+"="*(self.length*2-1)+colorama.Style.RESET_ALL+f"\n{colorama.Fore.MAGENTA}{remaining}{colorama.Fore.CYAN} guesses remaining.{colorama.Style.RESET_ALL}\n{format_keyboard()}\n> "
-        for remaining in range(self.length+1, 0, -1):
+
+        show = lambda: word * int(self.debug) + str(known) * int(self.debug) + colorama.Fore.BLUE + "=" * (
+                    self.length * 2 - 1) + colorama.Style.RESET_ALL + "\n" + "\n".join(
+            guesses + [" ".join([WHITE + "-" + colorama.Style.RESET_ALL] * (self.length)) for _ in
+                       range(remaining)]) + "\n" + colorama.Fore.BLUE + "=" * (
+                                   self.length * 2 - 1) + colorama.Style.RESET_ALL + f"\n{colorama.Fore.MAGENTA}{remaining}{colorama.Fore.CYAN} guesses remaining.{colorama.Style.RESET_ALL}\n{format_keyboard()}\n> "
+        if self.attempts == -1:
+            attempts = self.length + 1
+        else:
+            attempts = self.attempts
+        for remaining in range(attempts, 0, -1):
             while True:
-                i+=1
+                i += 1
                 inp = input(show()).lower()
                 if not inp in self.words:
                     utils.clear()
-                    print(colorama.Fore.RED+"You're an idiot >:(((. That isn't a valid word."+colorama.Style.RESET_ALL)
+                    print(
+                        colorama.Fore.RED + "You're an idiot >:(((. That isn't a valid word." + colorama.Style.RESET_ALL)
                     continue
                 if not check_hard(inp, known):
                     utils.clear()
-                    print(colorama.Fore.RED+"You're an idiot >:(((. You're on hard mode."+colorama.Style.RESET_ALL)
+                    print(colorama.Fore.RED + "You're an idiot >:(((. You're on hard mode." + colorama.Style.RESET_ALL)
                     continue
                 formatted = []
-                found = {c:0 for c in word}
+                found = {c: 0 for c in word}
                 for i, char in enumerate(inp):
                     if char in word:
                         found[char] += 1
                         if found[char] > word.count(char):
-                            formatted.append(GREY+char+colorama.Style.RESET_ALL)
+                            formatted.append(GREY + char + colorama.Style.RESET_ALL)
                             continue
                     if char == word[i]:
                         upgrade_keyboard(char, GREEN)
-                        formatted.append(GREEN+char+colorama.Style.RESET_ALL)
+                        formatted.append(GREEN + char + colorama.Style.RESET_ALL)
                         known.append((i, char))
                     elif char in word:
                         upgrade_keyboard(char, YELLOW)
-                        formatted.append(YELLOW+char+colorama.Style.RESET_ALL)
+                        formatted.append(YELLOW + char + colorama.Style.RESET_ALL)
                         known.append((-1, char))
                     else:
                         upgrade_keyboard(char, GREY)
-                        formatted.append(GREY+char+colorama.Style.RESET_ALL)
+                        formatted.append(GREY + char + colorama.Style.RESET_ALL)
                 guesses.append(" ".join(formatted))
                 if inp == word:
                     print(show())
-                    print(GREEN+"You actually did it :)))"+colorama.Style.RESET_ALL)
+                    print(GREEN + "You actually did it :)))" + colorama.Style.RESET_ALL)
                     return
                 utils.clear()
                 break
 
-        input(YELLOW+f"You used all of your guesses :((((. The word was \"{word}\""+colorama.Style.RESET_ALL)
+        input(YELLOW + f"You used all of your guesses :((((. The word was \"{word}\"" + colorama.Style.RESET_ALL)
